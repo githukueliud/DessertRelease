283184822e5077cf4d2098b87d9a416935ac1107
dev_eliud <githukueliud@gmail.com>
2023-09-05 21:50:09 -0500

    User repository preference is made available using manual DI

--- app/src/main/java/com/example/dessertrelease/DessertReleaseApplication.kt
+++ app/src/main/java/com/example/dessertrelease/DessertReleaseApplication.kt
@@ -1,8 +1,24 @@
 package com.example.dessertrelease
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.dessertrelease.data.local.UserPreferenceRepository
 
+
+private const val LAYOUT_PREFERENCE_NAME = "layout_preferences"
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
+    name = LAYOUT_PREFERENCE_NAME
+)
 
 //This is the container for the DataStore
 class DessertReleaseApplication: Application() {
+    lateinit var userPreferenceRepository: UserPreferenceRepository
+
+    override fun onCreate() {
+        super.onCreate()
+        userPreferenceRepository = UserPreferenceRepository(dataStore)
+    }
 }
